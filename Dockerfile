b5cce243c7adb9988a9e854c297497f513edbe0d
rhiaro <amy@rhiaro.co.uk>
2020-04-02 17:13:21 +0200

    Tidy up crawler infra

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alephdata/memorious:latest
 
-COPY setup.py /crawlers/
-COPY src /crawlers/src
-RUN pip3 install -q -e /crawlers
-COPY config /crawlers/config
+COPY setup.py /oroi-scrape/
+COPY oroi-scrape/scrapers /oroi-scrape/scrapers
+RUN pip3 install -q -e /oroi-scrape
+COPY oroi-scrape/config /oroi-scrape/config
--- setup.py
+++ setup.py
@@ -2,26 +2,19 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='oroi-scrape',
-    version='0.1',
-    author='Amy Guy',
-    author_email='amy.guy@opendataservices.coop',
-    url='https://github.com/opendataservices/oroi-scrape',
-    license='MIT',
+    name="oroi-scrape",
+    version="0.1",
+    author="Amy Guy",
+    author_email="amy.guy@opendataservices.coop",
+    url="https://github.com/opendataservices/oroi-scrape",
+    license="MIT",
     classifiers=[],
-    keywords='',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    keywords="",
+    packages=find_packages(exclude=["ez_setup", "examples", "test"]),
     namespace_packages=[],
+    package_data={"": ["oroi-scrape/config/*"]},
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'memorious >= 1.5.4',
-        'datafreeze'
-    ],
-    entry_points={
-        'memorious.plugins': [
-            # 'example = example:init'
-        ]
-    }
+    install_requires=["memorious >= 1.5.4", "datafreeze"],
+    entry_points={},
 )
