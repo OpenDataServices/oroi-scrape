a8ae05949c189d1aefacf6180439644315ba2cc6
rhiaro <amy@rhiaro.co.uk>
2020-05-28 10:02:04 +0200

    formatting: run black

--- oroi-scrape/scrapers/bristol.py
+++ oroi-scrape/scrapers/bristol.py
@@ -5,6 +5,8 @@ import lxml
 bristol_register
 Get the register of interest and some data from a council member page
 """
+
+
 def get_register(context, data):
     with context.http.rehash(data) as result:
         if result.html is not None:
@@ -15,7 +17,9 @@ def get_register(context, data):
             for link in links:
                 if "register of interest" in link.text.lower():
                     url = "{}{}".format(context.get("base"), link.get("href"))
-                    context.emit(data={"member_name": name, "member_url": result.url, "url": url})
+                    context.emit(
+                        data={"member_name": name, "member_url": result.url, "url": url}
+                    )
 
 
 """
@@ -32,6 +36,8 @@ Sections are:
     8. Membership of organisations
     9. Gifts and hospitality
 """
+
+
 def parse_register(context, data):
 
     declaration_mapping = {
@@ -49,7 +55,7 @@ def parse_register(context, data):
     notes_mapping = {
         "5.": "Contract type: licenses to occupy land",
         "6.": "Contract type: corporate tenancies",
-        "8.": "Position type: membership of organisations"
+        "8.": "Position type: membership of organisations",
     }
 
     with context.http.rehash(data) as result:
@@ -91,7 +97,12 @@ def parse_register(context, data):
                             # All other tables are just rows with a single cell
                             answers = declaration.findall(".//td")
                             for answer in answers:
-                                output["description"] = answer.text_content().replace("\r","").replace("\n","").strip()
+                                output["description"] = (
+                                    answer.text_content()
+                                    .replace("\r", "")
+                                    .replace("\n", "")
+                                    .strip()
+                                )
                                 context.emit(rule="store", data=output)
 
 
@@ -99,6 +110,8 @@ def parse_register(context, data):
 bristol_meetings
 Parse the listing of meetings
 """
+
+
 def parse_meetings(context, data):
     url_base = "https://democracy.bristol.gov.uk/"
     with context.http.rehash(data) as result:
@@ -109,7 +122,9 @@ def parse_meetings(context, data):
                 meeting_section_ele = declaration_list.getprevious()
                 meeting_ele = meeting_section_ele.getprevious()
 
-                meeting_link = "{}{}".format(url_base, meeting_ele.find(".//a").get("href"))
+                meeting_link = "{}{}".format(
+                    url_base, meeting_ele.find(".//a").get("href")
+                )
                 meeting_info = meeting_ele.find(".//a").text_content().split(" - ")
                 meeting_date = meeting_info[0]
                 meeting_group = meeting_info[1]
@@ -125,11 +140,13 @@ def parse_meetings(context, data):
                     output = {
                         "source": meeting_link,
                         "declared_date": meeting_date.strip(),
-                        "declared_to": "Bristol City Council ({})".format(meeting_group),
+                        "declared_to": "Bristol City Council ({})".format(
+                            meeting_group
+                        ),
                         "member_name": member_name.strip(),
                         "member_url": member_url,
                         "interest_type": "positions",
-                        "description": description.strip()
+                        "description": description.strip(),
                     }
 
                     context.emit(data=output)
